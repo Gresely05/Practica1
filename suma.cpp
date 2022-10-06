f04ab24c8e4d83efa5a129ce9e1a80fbc9221afe
Gresely05 <jordan.gresely@gmail.com>
2022-10-06 09:14:54 -0500

    Actividad de suma

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+//Creado por stalin
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float NACJ_x,NACJ_y,NACJ_z;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>NACJ_x>>NACJ_y;
+ NACJ_z=NACJ_x+NACJ_y;
+ cout<<"El resutado es : ";
+ cout<<NACJ_z<<endl;
+
+ return 0;
+
+}
